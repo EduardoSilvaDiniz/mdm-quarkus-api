f602d37f52b43740e07f37c58dfc8ddc7b6788b9
Eduardo Silva <eduardofox989@protonmail.com>
2025-04-24 17:41:04 -0300

    added: vo on records

--- src/main/java/br/org/mdm/record/Participant.java
+++ src/main/java/br/org/mdm/record/Participant.java
@@ -1,10 +1,20 @@
 package br.org.mdm.record;
 
 import br.org.mdm.entity.ParticipantEntity;
+import br.org.mdm.vo.Address;
+import br.org.mdm.vo.CPF;
+import br.org.mdm.vo.MaritalStatus;
 
-public record Participant(Long id, String name, String phone) {
+public record Participant(
+    Long id, String name, String phone, Address address, MaritalStatus maritalStatus, CPF cpf) {
 
-  static public Participant fromEntity(ParticipantEntity entity) {
-    return new Participant(entity.getId(), entity.getName(), entity.getPhone());
+  public static Participant fromEntity(ParticipantEntity entity) {
+    return new Participant(
+        entity.getId(),
+        entity.getName(),
+        entity.getPhone(),
+        entity.getAddress(),
+        entity.getMaritalStatus(),
+        entity.getCpf());
   }
 }
