026372bd63d0ed08fed81ed4b96845d1b1361a7b
Eduardo Silva <eduardofox989@protonmail.com>
2025-04-24 17:35:43 -0300

    added: vo on entity's

--- src/main/java/br/org/mdm/entity/ParticipantEntity.java
+++ src/main/java/br/org/mdm/entity/ParticipantEntity.java
@@ -1,13 +1,19 @@
 package br.org.mdm.entity;
 
-import br.org.mdm.record.Participant;
+import java.util.Objects;
+
 import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import java.util.Objects;
+
+import br.org.mdm.record.Participant;
+import br.org.mdm.vo.Address;
+import br.org.mdm.vo.CPF;
+import br.org.mdm.vo.MaritalStatus;
 
 @Entity
 @Table(name = "participants")
@@ -23,6 +29,18 @@ public class ParticipantEntity {
   @Column(nullable = false)
   private String phone;
 
+  @Embedded
+  @Column(nullable = false)
+  private Address address;
+
+  @Embedded
+  @Column(nullable = false)
+  private MaritalStatus maritalStatus;
+
+  @Embedded
+  @Column(nullable = false)
+  private CPF cpf;
+
   public ParticipantEntity() {}
 
   public ParticipantEntity(Participant entity) {
@@ -30,6 +48,13 @@ public class ParticipantEntity {
     this.name = entity.name();
     this.phone = entity.phone();
   }
+
+  // @PrePersist
+  // public void prePersist(
+  //     Integer number, String district, String street, MaritalStatus maritalStatus) {
+  //   this.address = new Address(number, district, street);
+  //   this.maritalStatus = maritalStatus;
+  // }
 
   @Override
   public int hashCode() {
@@ -51,38 +76,51 @@ public class ParticipantEntity {
     return Objects.equals(id, other.id);
   }
 
-	public Long getId() {
-		return id;
-	}
+  public Long getId() {
+    return id;
+  }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-	public String getName() {
-		return name;
-	}
+  public String getName() {
+    return name;
+  }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+  public void setName(String name) {
+    this.name = name;
+  }
 
-	public String getPhone() {
-		return phone;
-	}
+  public String getPhone() {
+    return phone;
+  }
 
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
 
-  // private Address address;
-  // private MaritalStatus maritalStatus;
-  // private CPF cpf;
+  public Address getAddress() {
+    return address;
+  }
 
-  // @PrePersist
-  // public void prePersist(Integer number, String district, String street, MaritalStatus
-  // maritalStatus){
-  // this.address = new Address(number, district, street);
-  // this.maritalStatus = maritalStatus;
-  // }
+  public void setAddress(Address address) {
+    this.address = address;
+  }
+
+  public MaritalStatus getMaritalStatus() {
+    return maritalStatus;
+  }
+
+  public void setMaritalStatus(MaritalStatus maritalStatus) {
+    this.maritalStatus = maritalStatus;
+  }
+
+  public CPF getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(CPF cpf) {
+    this.cpf = cpf;
+  }
 }
--- src/main/java/br/org/mdm/entity/PayEntity.java
+++ src/main/java/br/org/mdm/entity/PayEntity.java
@@ -1,6 +1,8 @@
 package br.org.mdm.entity;
 
+import br.org.mdm.vo.CPF;
 import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -26,41 +28,52 @@ public class PayEntity {
   @Column(nullable = false)
   private Integer value;
 
-	public Long getId() {
-		return id;
-	}
+  @Embedded
+  @Column(nullable = false)
+  private CPF cpf;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public LocalDateTime getPayDay() {
-		return payDay;
-	}
-
-	public void setPayDay(LocalDateTime payDay) {
-		this.payDay = payDay;
-	}
-
-	public LocalDate getMonthReference() {
-		return monthReference;
-	}
-
-	public void setMonthReference(LocalDate monthReference) {
-		this.monthReference = monthReference;
-	}
-
-	public Integer getValue() {
-		return value;
-	}
-
-	public void setValue(Integer value) {
-		this.value = value;
-	}
-
-  // private CPF cpf;
   // @PrePersist
   // public void prePersist(){
   // 	this.payDay = LocalDateTime.now();
   // }
+
+  public CPF getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(CPF cpf) {
+    this.cpf = cpf;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public LocalDateTime getPayDay() {
+    return payDay;
+  }
+
+  public void setPayDay(LocalDateTime payDay) {
+    this.payDay = payDay;
+  }
+
+  public LocalDate getMonthReference() {
+    return monthReference;
+  }
+
+  public void setMonthReference(LocalDate monthReference) {
+    this.monthReference = monthReference;
+  }
+
+  public Integer getValue() {
+    return value;
+  }
+
+  public void setValue(Integer value) {
+    this.value = value;
+  }
 }
